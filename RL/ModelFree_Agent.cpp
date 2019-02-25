00516425327c92d569386cdc552f9265734db5b0
daddabarba <davidebarbieri97@gmail.com>
2019-02-25 02:01:50 +0100

    added ModelFree_Agent interface

---
+++ RL/ModelFree_Agent.cpp
@@ -0,0 +1,10 @@
+//
+// Created by daddabarba on 25/02/19.
+//
+
+#include "ModelFree_Agent.hpp"
+#include "Defs.hpp"
+
+RLIB_INTERFACES::ModelFree_Agent::ModelFree_Agent(int state_space_size) :
+        RL_Agent(state_space_size)
+{};
---
+++ RL/ModelFree_Agent.hpp
@@ -0,0 +1,24 @@
+//
+// Created by daddabarba on 25/02/19.
+//
+
+#ifndef RLIB_MODELFREE_AGENT_HPP
+#define RLIB_MODELFREE_AGENT_HPP
+
+#include "Defs.hpp"
+#include "RL_Agent.hpp"
+
+RLIB_INTERFACES_START
+
+class ModelFree_Agent : public RL_Agent {
+
+public:
+
+    ModelFree_Agent(int state_space_size);
+
+};
+
+RLIB_INTERFACES_END
+
+
+#endif //HSRRL_MODELFREE_AGENT_HPP
