375f7579fa3e8f1d9782e363754599fc61a87792
daddabarba <davidebarbieri97@gmail.com>
2019-02-26 22:22:14 +0100

    added setting of U in Q_Agent (standard)

--- RLIB/Bases/Q_Agent.cpp
+++ RLIB/Bases/Q_Agent.cpp
@@ -12,7 +12,14 @@ RLIB_BASES::Q_Agent::Q_Agent(Space_Size state_space_size, Space_Size action_spac
         Transition_Learning_Agent(state_space_size, action_space_size),
         Q(REACT_CONC::make_variable<arma::Mat<double>>(arma::Mat<double>((arma::uword)state_space_size, (arma::uword)action_space_size))),
         U(REACT_CONC::make_variable<arma::Mat<double>>(arma::Mat<double>(state_space_size, 1)))
-{}
+{
+
+    // Set up U as the expected value of Q, with respect to Pi
+    REACT_CONC::make_operator_set([](arma::Mat<double> Q, arma::Mat<double> Pi) -> arma::Mat<double>{
+        return Q*Pi;
+    }, getU(), getQ(), getP());
+
+}
 
 auto RLIB_BASES::Q_Agent::U_fun(State state) -> double {
     return ((arma::Mat<double>)*getU()).at((arma::uword)state);
